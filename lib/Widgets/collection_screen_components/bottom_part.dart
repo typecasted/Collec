4729a1331cb4d358cf99cb120f72c4217ed1d248
Kunal Pithadiya <kunal.pithadiya@techniversesoftware.com>
2021-08-04 12:42:04 +0530

    refactored the code

---
+++ lib/Widgets/collection_screen_components/bottom_part.dart
@@ -0,0 +1,12 @@
+
+
+import 'package:collec/UI/Components/collection_screen_components/preview_photo_page_view.dart';
+import 'package:collec/UI/Screens/photo_screen.dart';
+import 'package:collec/utils/constants.dart';
+import 'package:collec/utils/size_config.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'other_components.dart';
+
